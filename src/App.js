89065a1175ada2d453ee1dab4f6b3edaa5972c89
LazyCoder-1506 <pranayan1506@gmail.com>
2023-05-27 21:17:32 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import ExercisePage from './components/Exercises/ExercisePage';
@@ -6,7 +7,10 @@ import AddWorkoutPage from './components/Workouts/AddWorkout/AddWorkoutPage';
 function App() {
   return (
     <div className='bg-slate-900 text-slate-300 min-h-screen'>
-      <AddWorkoutPage />
+      <Routes>
+        <Route path='/' element={<AddWorkoutPage />} />
+        <Route path='/exercises' element={<ExercisePage />} />
+      </Routes>
     </div>
   );
 }
